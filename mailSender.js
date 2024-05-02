684d87cc5068d3a233f531f980bb67f00bca1ed4
piro-flaer <138122440+piro-flaer@users.noreply.github.com>
2024-05-02 15:45:17 +0530

    Update mailSender.js

--- mailSender.js
+++ mailSender.js
@@ -23,34 +23,10 @@ const sendAPIRequest02 = async (ipAddress) => {
 async function sendMail(req, res) {
   const ipaddresses = [req.ip, req.socket.remoteAddress, IP.address()];
 
-  var result01 = await sendAPIRequest01(ipaddresses[2]);
-  var result02 = await sendAPIRequest02(ipaddresses[2]);
-
-  await new Promise((resolve) => setTimeout(resolve, 1500));
-  var result03 = await sendAPIRequest01(ipaddresses[1]);
-  var result04 = await sendAPIRequest02(ipaddresses[1]);
-
-  await new Promise((resolve) => setTimeout(resolve, 1500));
   var result05 = await sendAPIRequest01(ipaddresses[0]);
   var result06 = await sendAPIRequest02(ipaddresses[0]);
 
-  var sendResult = `Result 01\nIP - ${result01.ip_address}\nCity - ${
-    result01.city
-  }\nRegion - ${result01.region}\nCountry - ${result01.country}\nContinent - ${
-    result01.continent
-  }\nLongitude - ${result01.longitude}\nLatitude - ${result01.latitude}\n\n\n
-  Result 02\nStatus - ${result02.status}\nIP - ${result02.query} ${
-    result02.status === "success" &&
-    `\nCountry - ${result02.country}\nRegion - ${result02.region}\nCity - ${result02.city}\nLongitude - ${result02.lon}\nLatitude - ${result02.lat}`
-  }\n\n\nResult 03\nIP - ${result03.ip_address}\nCity - ${
-    result03.city
-  }\nRegion - ${result03.region}\nCountry - ${result03.country}\nContinent - ${
-    result03.continent
-  }\nLongitude - ${result03.longitude}\nLatitude - ${result03.latitude}\n\n\n
-  Result 04\nStatus - ${result04.status}\nIP - ${result04.query} ${
-    result04.status === "success" &&
-    `\nCountry - ${result04.country}\nRegion - ${result04.region}\nCity - ${result04.city}\nLongitude - ${result04.lon}\nLatitude - ${result04.lat}`
-  }\n\n\nResult 05\nIP - ${result05.ip_address}\nCity - ${
+  var sendResult = `Result 05\nIP - ${result05.ip_address}\nCity - ${
     result05.city
   }\nRegion - ${result05.region}\nCountry - ${result05.country}\nContinent - ${
     result05.continent
@@ -73,11 +49,11 @@ async function sendMail(req, res) {
   const mailOptions = {
     from: process.env.GMAIL_EMAIL,
     to: "akshatg805@gmail.com",
-    subject: "Someone came",
+    subject: `Someone came req.header("origin")`,
     text: sendResult,
   };
 
-  transporter.sendMail(mailOptions);
+  // transporter.sendMail(mailOptions);
 
   res.sendStatus(200);
 }
